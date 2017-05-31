7d819392b48c709a9fd84cebd38acd1df3f2ae3d
Alec Gibson <aleckgibson@googlemail.com>
2017-05-31 08:19:55 +0100

    Remove spurious typo

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function definePaths() {
     });
 
     server.post(TV_BASE_PATH, function (request, response) {
-        var intent = (request.body && request.body.request && request.body.request.intent) || {};s
+        var intent = (request.body && request.body.request && request.body.request.intent) || {};
         var intentName = intent.name || '';
 
         var alexaResponse;
